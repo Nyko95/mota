4bcdcc54cf40ef97a211956099cb636dd27183b9
Nicolas Guapo <nicolas.lecoeur.guapo@gmail.com>
2024-05-14 15:46:30 +0200

    modif sur modale sur le lien contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-/*GESTION DE L'APPARITION ET DISPARITION DE LA MODALE DE CONTACT*/
+/*OUVERTURE DE LA MODALE SUR LE LIEN "CONTACT"*/
 
 jQuery(document).ready(function ($) {
-  // Ouvrir la modale au clic sur le lien de contact
-  $(".contact-modal-link").on("click", function (e) {
+  // Ouvrir la modale au clic sur le lien "Contact" dans le header
+  $(".menu-item-17").on("click", function (e) {
     e.preventDefault();
     $("#modal-contact").fadeIn();
   });
@@ -10,15 +10,5 @@ jQuery(document).ready(function ($) {
   // Fermer la modale au clic sur le bouton de fermeture
   $(".modal-close").on("click", function () {
     $("#modal-contact").fadeOut();
-  });
-
-  // Fermer la modale au clic en dehors de la modale
-  $(document).on("click", function (e) {
-    if (
-      !$(e.target).closest(".modal-content").length &&
-      !$(e.target).is(".contact-modal-link")
-    ) {
-      $("#modal-contact").fadeOut();
-    }
   });
 });
