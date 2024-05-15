34926b1a08cfdeeb9c2a6255482ba49d48f77295
Nicolas Guapo <nicolas.lecoeur.guapo@gmail.com>
2024-05-15 11:32:40 +0200

    ajout de la modale de contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-/*OUVERTURE DE LA MODALE SUR LE LIEN "CONTACT"*/
+/*OUVERTURE DE LA MODALE SUR LE LIEN "CONTACT" ET FERMETURE*/
 
 jQuery(document).ready(function ($) {
   // Ouvrir la modale au clic sur le lien "Contact" dans le header
   $(".menu-item-17").on("click", function (e) {
     e.preventDefault();
+    e.stopPropagation(); // Empêche la propagation de l'événement de clic
     $("#modal-contact").fadeIn();
   });
 
   // Fermer la modale au clic sur le bouton de fermeture
   $(".modal-close").on("click", function () {
-    $("#modal-contact").fadeOut();
+    $(".modal").fadeOut();
+  });
+
+  // Fermer la modale au clic en dehors de celle-ci
+  $(document).on("click", function (e) {
+    if (
+      !$(e.target).closest(".modal-content").length &&
+      !$(e.target).is(".menu-item-17")
+    ) {
+      $(".modal").fadeOut();
+    }
   });
 });
--- template_parts/modal-contact.php
+++ template_parts/modal-contact.php
@@ -1,10 +1,10 @@
-<!-- Modale de contact -->
-
 <div id="modal-contact" class="modal">
     <div class="modal-content">
         <!-- Contenu de la modale -->
+        <img src="/wp-content/themes/mota/assets/images/contact.png" alt="contact" class="contact">
         <span class="modal-close">&times;</span>
-        <h2>Contact</h2>
-        <?php echo do_shortcode('[contact-form-7 id="959f2ed" title="modale de contact"]'); ?>
+        <div class="form-container">
+            <?php echo do_shortcode('[contact-form-7 id="959f2ed" title="modale de contact"]'); ?>
+        </div>
     </div>
 </div>
